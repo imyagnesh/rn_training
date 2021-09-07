064386195c9b873af100ac9bda54285e974c8412
Jayanthi Dhanabalan <jayanthi.dhanabalan@olamnet.com>
2021-09-08 01:58:19 +0530

    add :  - Promise implementation with async, await, all, allsettled, any and race  - Group by age and sort by age  - Reduce loop as every

---
+++ PromisesTask.js
@@ -0,0 +1,67 @@
+
+const promise1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 1 Resolved')
+        }, 1500);
+    });
+}
+
+const promise2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 2 Resolved')
+        }, 1000);
+    });
+}
+
+const promise3 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 3 Resolved')
+        }, 2500);
+    });
+}
+
+const allSettledPromise = async () => {
+    try {
+        const allSettledValues = await Promise.allSettled([promise1(), promise2(), promise3()]);
+        console.log(`allSettledValues : ${allSettledValues}`);
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+const allPromise = async () => {
+    try {
+        const allValues = await Promise.all([promise1(), promise2(), promise3()]);
+        console.log(`allValues : ${allValues}`)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+const racePromise = async () => {
+    try {
+        const raceResult = await Promise.race([promise1(), promise2(), promise3()]);
+        console.log(`RaceValues : ${raceResult}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const anyPromise = async () => {
+    try {
+
+        const anyResult = await Promise.any([promise1(), promise2(), promise3()]);
+        console.log(`anyResult : ${anyResult}`)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+allPromise();
+allSettledPromise();
+racePromise();
+anyPromise();
