b8fe3c95623f2f0b458b81b35e5805d43b46d7a5
pravin.sivalingam <pravin.sivalingam@olamnet.com>
2021-09-08 00:22:04 +0530

    AsyncAwait

--- AsyncTask.js
+++ AsyncTask.js
@@ -17,7 +17,7 @@ const p3 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('p3 resolved');
-        }, 1000);
+        }, 2500);
     });
 };
 
@@ -34,4 +34,46 @@ console.log(err);
 }
 };
 
+const syncAllSettled = async () => {
+    try{
+    const res = await Promise.allSettled([
+        p1(),
+        p2(),
+        p3()
+    ]);
+    console.log(res);  
+} catch(err){
+console.log(err);
+}
+};
+
+const syncRace = async () => {
+    try{
+    const res = await Promise.race([
+        p1(),
+        p2(),
+        p3()
+    ]);
+    console.log(res);  
+} catch(err){
+console.log(err);
+}
+};
+
+const syncAny = async () => {
+    try{
+    const res = await Promise.any([
+        p1(),
+        p2(),
+        p3()
+    ]);
+    console.log(res);  
+} catch(err){
+console.log(err);
+}
+};
+
 syncAll();
+syncAllSettled();
+syncRace();
+//syncAny();
