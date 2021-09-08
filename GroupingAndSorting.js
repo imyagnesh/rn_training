50f60ebbac91628161125db740d5d09610551b52
pravin.sivalingam <pravin.sivalingam@olamnet.com>
2021-09-08 23:40:45 +0530

    Task1

--- GroupingAndSorting.js
+++ GroupingAndSorting.js
@@ -34,10 +34,18 @@ const users = [
     const group = users.reduce((p,c)=>{
 const ageKey = Math.floor(c.age/10);
 const key = `${ageKey}0-${ageKey}9`;
-(p[key]=p[key] || []).push(c);
-p[key].sort((a,b) => {
-    return a.age - b.age
-        });
+(p[key]= p[key] || []).push(c);
+
+// p[key].sort((a,b) => {
+//     return a.age - b.age
+//         });
+for(let i=1; i<p[key].length; i++){
+    if (p[key][i - 1].age > p[key][i].age) {
+        var tmp = p[key][i - 1];
+        p[key][i - 1] = p[key][i];
+        p[key][i] = tmp;
+      }
+}
 return p;
     }, {})
 
