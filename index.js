2227d84e52f4b08abf2bf5b971376114db337138
pravin.sivalingam <pravin.sivalingam@olamnet.com>
2021-09-14 11:32:32 +0530

    Weather

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+
+// ToDo App
+// import App from './app';
+
+// Weather App
+import App from './weatherApp';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ weather.js
@@ -0,0 +1,3 @@
+const weatherData = [{ place: 'Chennai', temp: 32 }, { place: 'Kanyakumari', temp: 30 }];
+
+export default weatherData;
