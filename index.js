5415dbceab52ed173c5ee36c6b7a65b7b386d062
Dencer <Dencer@INCHN-Mac2363.local>
2021-09-14 22:31:23 +0530

    Task: weather Api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-// import WeatherApp from './weather';
+// import App from './app';
+import WeatherApp from './weather';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <WeatherApp />,
+  document.getElementById('root'),
+);
