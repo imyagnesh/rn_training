465afab205be01ed7718936b0e9866ced69e4e45
baskarankannan <baskmca@gmail.com>
2021-09-14 12:12:30 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import WeatherApp from './weather';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
