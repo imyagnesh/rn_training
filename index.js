80cf11a0889c3cdbf1639b9f7cccba33a8567f4e
baskarankannan <baskmca@gmail.com>
2021-09-15 09:49:08 +0530

    Weather App Auto Search

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-// import WeatherApp from './weather';
+// import App from './app';
+import WeatherApp from './weather';
 
 ReactDOM.render(<WeatherApp />, document.getElementById('root'));
--- weather/index.js
+++ weather/index.js
@@ -1,23 +1,62 @@
-import React, { PureComponent } from 'react';
+import React, { createRef, PureComponent } from 'react';
+import './weather.scss';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
 
 class WeatherApp extends PureComponent {
+    weatherInputRef = createRef(null);
+
     state = {
-        cities: [
-            {
-                name: 'ahmedabad',
-                temp: 38,
-            },
-            {
-                name: 'pune',
-                temp: 30,
-            },
-        ],
+        cities: [],
+        filterCities: [],
     };
 
+    componentDidMount = async () => {
+        try {
+            const res = await fetch(
+                'http://localhost:3000/weather',
+            );
+            const json = await res.json();
+            console.log(json);
+            this.setState({
+                cities: json,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    checkWeather = () => {
+        const val = this.weatherInputRef.current.value;
+        const json = this.state.cities.filter(x => {
+            if (x.city.startsWith(val)) {
+                return x;
+            }
+        });
+        this.setState({
+            filterCities: json,
+        });
+        console.log(json);
+    };
+    // https://dzone.com/articles/how-to-add-autocomplete-textbox-in-react-applicati
     render() {
+        const { weather, filterCities } = this.state;
+
         return (
-            <div>
-                <h1>Weather App</h1>
+            <div className="container">
+                <h2>Type a City:</h2>
+                <div className="wrapper">
+                    {/* <input type="text" ref={this.weatherInputRef} /> */}
+                    <Autocomplete className="pding" id="combo-box-demo" options={this.state.cities} getOptionLabel={option => option.Name} style={{ width: 300 }} renderInput={params => (< TextField {...params} label="Auto Complete" variant="outlined" fullWidth />)} />
+                    <button type="button" onClick={this.checkWeather}>
+                        Check Weather
+                    </button>
+                </div>
+                {filterCities.map(item => (
+                    <span key={item.id}>
+                        Today's weather in {item.city} is {item.temp}°C
+                    </span>
+                ))}
             </div>
         );
     }
