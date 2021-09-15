d23390a0e9eb5be0c1ece7ad9dbdc69a0159928e
sridhar-mari <sridhar.mari@olamnet.com>
2021-09-15 14:22:10 +1000

    Weather

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, createRef } from 'react';
+/*import React, { PureComponent, createRef } from 'react';
 import './app.scss';
 import TodoFilter from './todo/todoFilter';
 import TodoForm from './todo/todoForm';
@@ -225,7 +225,7 @@ class App extends PureComponent {
   }
 }
 
-export default App;
+export default App;*/
 
 // life cycle methods
 
@@ -276,3 +276,86 @@ export default App;
 
 // 2. componentDidCatch
 // log error on server
+import React, { PureComponent } from 'react';
+import './app.scss';
+
+class App extends PureComponent {
+  state = {
+    cities: [
+      {
+        id: new Date().valueOf(),
+        name: 'Chennai',
+        temp: 35,
+      },
+      {
+        id: new Date().valueOf() + 1,
+        name: 'Coimbatore',
+        temp: 28,
+      },
+      {
+        id: new Date().valueOf() + 2,
+        name: 'Cuddalore',
+        temp: 35,
+      },
+      {
+        id: new Date().valueOf() + 3,
+        name: 'Erode',
+        temp: 37,
+      },
+      {
+        id: new Date().valueOf() + 4,
+        name: 'Salem',
+        temp: 33,
+      },
+      {
+        id: new Date().valueOf() + 5,
+        name: 'Tirunelveli',
+        temp: 37,
+      },
+      {
+        id: new Date().valueOf() + 6,
+        name: 'Madurai',
+        temp: 36,
+      },
+      {
+        id: new Date().valueOf() + 7,
+        name: 'Tiruppur',
+        temp: 36,
+      },
+    ],
+  };
+
+  onSelectChange = e => {
+    const { cities } = this.state;
+    const value = e.target.value;
+    const city = cities.find(x => x.name === value);
+    this.setState({
+      weather: `Today's weather in ${city.name} is ${city.temp}°C`,
+    });
+  };
+
+  render() {
+    const { cities, weather } = this.state;
+
+    return (
+      <div className="container">
+        <h2>Choose a City:</h2>
+        <div className="wrapper">
+          <select
+            name="cities"
+            onChange={this.onSelectChange}>
+            {cities.map(item => (
+              <option key={item.id} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <button type="button">Check Weather</button>
+        </div>
+        <span>{weather}</span>
+      </div>
+    );
+  }
+}
+
+export default App;
