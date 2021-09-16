7d8f1a7ed8ea4981015265daed4617becc0cd9e5
Baskaran K <baskmca@gmail.com>
2021-09-16 08:39:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-// import WeatherApp from './weather';
+// import App from './app';
+import WeatherApp from './weather';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <WeatherApp />,
+  document.getElementById('root'),
+);
+
+// ReactDOM.render(
+//   <App initCounter={3} />,
+//   document.getElementById('root'),
+// );
